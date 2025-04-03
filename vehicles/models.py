fc8ae1a2291b4d7de0a0c98fdc1b35ebbba1273d
Pablo Alves Neri <pabloneri66@gmail.com>
2025-04-03 12:03:38 -0300

    Vehicles

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from costumers.models import Costumer
 
-# Create your models here.
+
+class VehicleType(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='Nome do Veículo',
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Descrição',
+    )
+
+    def __str__(self):
+        return self.name
+    
+
+class Vehicle(models.Model):
+    vehicle_type = models.ForeignKey(
+        VehicleType,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='Vehicles',
+        verbose_name='Tipo de Veículo',
+    )
+    #License Plate é Requisito Funcional.
+    license_plate = models.CharField(
+        max_length=10,
+        unique=True,
+        verbose_name='Placa',
+    )
+    #Má pratica. Fazer um objeto Brand...
+    brand = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True,
+        verbose_name='Marca',
+        )
+    model = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True,
+        verbose_name='Modelo',
+        )
+    color = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True,
+        verbose_name='Cor',
+        )
+    owner = models.ForeignKey(
+        Costumer,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='vehicles',
+        verbose_name='Proprietário',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Criado em',
+        )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name='Atualizado em',
+        )
