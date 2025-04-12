14676b1ceb7109dc58841b78567ccc90f4a50d2b
Pablo Alves Neri <pabloneri66@gmail.com>
2025-04-12 19:33:00 -0300

    Default Router

--- customers/urls.py
+++ customers/urls.py
@@ -1,1 +1,12 @@
 #Vamos Usar MVS para Crud Simples
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from customers.views import CustomerViewSet
+
+router = DefaultRouter()
+router.register('customers', CustomerViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
