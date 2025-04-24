8f84e3c751e1214f5947c7cf4d853d9b6908451c
Pablo Alves Neri <pabloneri66@gmail.com>
2025-04-24 13:23:43 -0300

    View - Url - Serializer

--- vehicles/serializers.py
+++ vehicles/serializers.py
@@ -1,1 +1,12 @@
 from vehicles.models import Vehicle, VehicleType
+from rest_framework import serializers
+
+class VehicleTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleType
+        fields = '__all__'
+
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
