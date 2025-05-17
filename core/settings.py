de4ea7427b5e3c03e04482f29800227c202e3008
Pablo Alves Neri <pabloneri66@gmail.com>
2025-05-17 12:27:07 -0300

    feat:Default Perm

--- core/settings.py
+++ core/settings.py
@@ -212,6 +212,12 @@ JAZZMIN_UI_TWEAKS = {
 # Django REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+        'rest_framework_simplejwt.authentication.JWTAuthentication', #classe Padrão SimpleJWT
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', #para nao deixar as nossas rotas públicas
+        'rest_framework.permissions.DjangoModelPermissions',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
 }
